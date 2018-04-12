42e78c30c68b2fe2705f729585ab70ec30292d61
japgroevemaker <joepgravemaker@gmail.com>
2018-04-12 20:00:01 +0200

    making an basic for an game

--- index.js
+++ index.js
@@ -1,22 +1,56 @@
-var express = require('express');
+var express = require('express')
+var ejs = require('ejs')
+var bodyParser = require('body-parser')
+var app = express()
 var socket = require("socket.io");
 
-//APP setup
-var app = express();
-var server = app.listen(4000, function() {
-  console.log('Executed at port 4000');
-})
+var arr = ['Gues the number between 1 and 10', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(arr);
+
+function shuffle(a) {
+  var j, x, i;
+
+  for (var i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+
+  return a;
+}
 
 //static files
+app.set('view engine', 'ejs')
+
 app.use(express.static('public'));
 
-//Socket setup
+app.get("/", function(req, res) {
+  res.render("index");
+})
+
+var server = app.listen(4000, function() {
+  console.log('Executed at port 4000');
+});
+
 var io = socket(server);
 
 io.on('connection', function(socket) {
   console.log('Made socket connection', socket.id);
 
   socket.on('chat', function(data) {
+    data.arr = shuffle(arr);
     io.sockets.emit('chat', data);
+  });
+
+  socket.on('guessNumber', function(data) {
+    console.log('chat');
+    io.sockets.emit('guessNumber', data);
   })
+
+  socket.on('typing', function(data){
+      socket.broadcast.emit('typing', data)
+    });
+
 });
--- public/chat.js
+++ public/chat.js
@@ -5,7 +5,9 @@ var socket = io.connect('http://localhost:4000');
 var message = document.getElementById('message');
     handle = document.getElementById('handle'),
     btn = document.getElementById('send'),
-    output = document.getElementById('output');
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback'),
+    guessNumber = document.createElement('div');
 
 // Emit events
 
@@ -16,7 +18,27 @@ btn.addEventListener('click', function() {
   });
 });
 
+message.addEventListener('keypress', function() {
+  socket.emit('typing', handle.value);
+})
+
 //Listen for events
 socket.on('chat', function(data) {
+  checkGuessNumber(data);
+  feedback.innerHTML = "";
   output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
-})
+});
+
+socket.on('typing', function(data){
+  feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
+
+function checkGuessNumber(data) {
+  console.log(data);
+    if (data.message.startsWith('/number', 0)) {
+      console.log('werkt');
+      output.appendChild(guessNumber)
+      guessNumber.innerHTML = '<p><i>' + data.arr[0] + '</i></p>';
+      console.log(data.arr[0]);
+    }
+}
